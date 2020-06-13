57304f414803f50072fd26c03e90b7c8b54269e3
andmorefine <andmorefine@gmail.com>
2020-06-14 02:17:36 +0900

    test

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.PesponseWriter, r *http.Request) {
+	http.
+	title := r.URL.Pasth[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+	// p := &Page{Title: "test", Body: []byte("this is test page")}
+	// p.save()
+
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
